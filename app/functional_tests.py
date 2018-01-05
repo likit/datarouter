18d7c97e4215851ebfd812d0fd1bfd4f34322140
Likit Preeyanon <likit.pre@mahidol.edu>
2018-01-05 15:12:47 +0700

    added comments

--- app/functional_tests.py
+++ app/functional_tests.py
@@ -1,9 +1,18 @@
 import requests
+'''Test registration of organization.
+    parameters:
+    - name = name of organization
+    - type = type of organization: private, public..
+    - postal_code = postcal code
+    - address = street address
+'''
 
+# test no parameters
 r = requests.post('http://localhost:5550/users/orgs/',
         json={})
 assert r.status_code == 400
 
+# test no name
 r = requests.post('http://localhost:5550/users/orgs/',
         json={'postal_code': '10150'})
 assert r.status_code == 400
