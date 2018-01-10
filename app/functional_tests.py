d4568a1f7e98283bb11960b46b9fc2719860a2d5
Sittha Maneemas <sittha_maneemas@yahoo.com>
2018-01-10 14:46:09 +0700

    add no postal code testing

--- app/functional_tests.py
+++ app/functional_tests.py
@@ -16,9 +16,15 @@ r = requests.post('http://{0}:{1}/users/orgs/'.format(host_ip, port),
         json={})
 assert r.status_code == 400
 
-# test no name
+# test no organization name
 r = requests.post('http://{0}:{1}/users/orgs/'.format(host_ip, port),
         json={'postal_code': '10150'})
+assert r.status_code == 400
+assert r.json()['message'] == 'Required parameters missing'
+
+# test no postal code
+r = requests.post('http://{0}:{1}/users/orgs/'.format(host_ip, port),
+        json={'org_name': 'Mahidol'})
 assert r.status_code == 400
 assert r.json()['message'] == 'Required parameters missing'
 
