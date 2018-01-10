d83d9fb683bf27f1009533817026d81277a7848f
Sittha Maneemas <sittha_maneemas@yahoo.com>
2018-01-10 15:57:52 +0700

    added org type testing

--- app/functional_tests.py
+++ app/functional_tests.py
@@ -18,13 +18,19 @@ assert r.status_code == 400
 
 # test no organization name
 r = requests.post('http://{0}:{1}/users/orgs/'.format(host_ip, port),
-        json={'postal_code': '10150'})
+        json={'postal_code': '10150', 'org_type': 'Academic'})
+assert r.status_code == 400
+assert r.json()['message'] == 'Required parameters missing'
+
+# test no organization type
+r = requests.post('http://{0}:{1}/users/orgs/'.format(host_ip, port),
+        json={'postal_code': '10150', 'org_name': 'Mahidol'})
 assert r.status_code == 400
 assert r.json()['message'] == 'Required parameters missing'
 
 # test no postal code
 r = requests.post('http://{0}:{1}/users/orgs/'.format(host_ip, port),
-        json={'org_name': 'Mahidol'})
+        json={'org_name': 'Mahidol', 'org_type': 'Academic'})
 assert r.status_code == 400
 assert r.json()['message'] == 'Required parameters missing'
 
