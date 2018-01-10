4a82d6542a5aa1c442db61eb7908eb071e52a7d9
Likit Preeyanon <likit.pre@mahidol.edu>
2018-01-10 10:29:14 +0700

    replaced hardcoded host IP and port with environmental variables

--- app/functional_tests.py
+++ app/functional_tests.py
@@ -1,4 +1,5 @@
 import requests
+import os
 '''Test registration of organization.
     parameters:
     - name = name of organization
@@ -7,13 +8,16 @@ import requests
     - address = street address
 '''
 
+host_ip = os.environ.get('HOST_IP', 'localhost')
+port = os.environ.get('PORT', '5550')
+
 # test no parameters
-r = requests.post('http://localhost:5550/users/orgs/',
+r = requests.post('http://{0}:{1}/users/orgs/'.format(host_ip, port),
         json={})
 assert r.status_code == 400
 
 # test no name
-r = requests.post('http://localhost:5550/users/orgs/',
+r = requests.post('http://{0}:{1}/users/orgs/'.format(host_ip, port),
         json={'postal_code': '10150'})
 assert r.status_code == 400
 assert r.json()['message'] == 'Required parameters missing'
