a65313d27269e63e18f3be9a4614c6b4e563346b
Sittha Maneemas <sittha_maneemas@yahoo.com>
2018-03-05 14:18:23 +0700

    User in JSON

---
+++ app/user.py
@@ -0,0 +1,59 @@
+# user info in JSON
+
+user = [
+    {
+    'id':'001',
+    'person': {
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'email':'john_doe@test.com',
+        'lisence_id':'123456789',
+        'occupation':'researcher',
+        'position':'head of laboratory',
+        'date_of_birth(yyyy/mm/dd)':'1988/12/31'
+        'gender':'male'
+    },
+    'organisation':{
+        'name':'Faculty of Medical Technology',
+        'type':'Academic',
+        'department':'Microbiology',
+        'size':'-',
+        'address':{
+            'house_number':'9',
+            'street':'Phutthamonthon Sai 4',
+            'subdistrict':'Salaya',
+            'district':'Phutthamonthon',
+            'province':'Nakhonpahom',
+            'postal_code':'73170'
+        }
+    }
+},
+{
+    'id':'002',
+    'person': {
+        'first_name': 'Jane',
+        'last_name': 'Doe',
+        'email':'jane_doe@test.com',
+        'lisence_id':'987654321',
+        'occupation':'Medical technologist',
+        'position':'head of laboratory',
+        'date_of_birth(yyyy/mm/dd)':'1988/10/21'
+        'gender':'female'
+    },
+    'organisation':{
+        'name':'Ramathibodi Hospital',
+        'type':'laboratory',
+        'department':'Microbiology',
+        'size':'1000 beds',
+        'address':{
+            'house_number':'270',
+            'street':'Rama VI road',
+            'subdistrict':'Thun Phayathai',
+            'district':'Ratchatewi',
+            'province':'Bangkok',
+            'postal_code':'10400'
+        }
+    }
+}
+
+]
